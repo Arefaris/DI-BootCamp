c87f8b39dfefd00563cf8941e8fbdaa41b903b13
Arefaris <111435464+Arefaris@users.noreply.github.com>
2025-05-12 15:27:12 +0300

    daily challenge

--- W1/D2/Daily Challenge/challenge.py
+++ W1/D2/Daily Challenge/challenge.py
@@ -8,3 +8,12 @@ for n in range(1, length+1):
     numbersList.append(number * n)
 print(numbersList)
 
+#challenge 2
+string = input("String?")
+sentence = ""
+for letter in string:
+    if letter in sentence:
+        pass
+    else:
+        sentence += letter
+print(sentence)
