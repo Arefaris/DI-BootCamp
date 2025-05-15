06ee27d2c3426d3ee0b26c49385bb5942e5474d1
Arefaris <arefaris6969@gmail.com>
2025-05-16 00:29:47 +0300

    game works

--- W1/D5/miniProject/main.py
+++ W1/D5/miniProject/main.py
@@ -1,8 +1,8 @@
 import random
+
 game_board = [[" ", " ", " "], 
               [" ", " ", " "], 
               [" ", " ", " "]]
-
 
 def main():
     
@@ -10,34 +10,66 @@ def main():
     play()
     
 def play():
+    
     turn = ["CPU", "PLAYER"]
     turn = random.choice(turn)
     win = False
     player_symbol = "X"
     
     while win != True:
-        player = []
-        
+       
+        print(turn)
+        current_turn = turn
         if turn == "CPU":
             player_symbol = "O"
-            player_input(random.randint(0,2), random.randint(0, 2), player_symbol)
-            turn == "PLAYER"
+            placed = False
+            
+            #loop to check if space is free
+            while not placed:
+                col = random.randint(0, 2)
+                row = random.randint(0, 2)
+                if game_board[col][row] == " ":
+                    player_input(col, row, player_symbol)
+                    placed = True
+            turn = "PLAYER"
+            
         else: 
-            user_column = int(input("column "))
-            user_row = int(input("row "))
+            user_column = input("column ")
+            user_row = input("row ")
+
+            if not (user_column.isdigit() and user_row.isdigit()):
+                print("Invalid input. Please enter numbers.")
+                continue
+
+            user_column = int(user_column)
+            user_row = int(user_row)
             
             ### check if column and rows are in valid range
             if user_column > 0 and user_column <= 3 and user_row >= 0 and user_row <= 3:
                 user_column -= 1
                 user_row -= 1
                 player_symbol = "X"
-                player_input(user_column, user_row, player_symbol)
-                turn == "CPU"
+                
+                if game_board[user_column][user_row] == " ":
+                    player_input(user_column, user_row, player_symbol)
+                    turn = "CPU"
+                else:
+                    print("not empty")
+                    continue
             else:
                 print("Your input is out of range")
+                continue
                 
-        win = check_win(game_board, player)
+        tie = check_for_tie(game_board)        
+        win = check_win(game_board, player_symbol)
         display_board(game_board)
+        
+        if win:
+            print(f"{current_turn} wins the game!")
+            break
+        if tie:
+            print("TIE, game over")
+            break
         
 def check_for_tie(game_board):
     tie = 0
@@ -59,16 +91,19 @@ def display_board(game_board):
 def player_input(column, row, symbol):
     
     ### check if space is empty
+    
     if game_board[column][row] == " ":
         game_board[column][row] = symbol
     else: 
         print("not empty")
+    
 
-def check_win(game_board, player):
-        current_row = []
+def check_win(game_board, symbol):
         diagonal_track_left = []
         diagonal_track_right = []
-        symbol = player[2]
+        
+        # xxx or ooo depending on CPU/PLAYER
+        sequence = symbol + symbol + symbol 
         
         #for loop in range 3 (lenght of our columns)
         for column in range(3):
@@ -81,32 +116,19 @@ def check_win(game_board, player):
             
             
             if len(diagonal_track_left) == 3:
-                if "XXX" == "".join(diagonal_track_left):
-                    print("You won diagonal win")
+                if sequence == "".join(diagonal_track_left):
                     return True
                 elif len(diagonal_track_right) == 3:
-                    if "XXX" == "".join(diagonal_track_right):
-                        print("You won diagonal win")
+                    if sequence == "".join(diagonal_track_right):
                         return True
-                    
                     
             #if every char in current column is the same, its a win condition
             if game_board[column][0] == symbol and game_board[column][1] == symbol and game_board[column][2] == symbol:
-                print("You won column win")
                 return True
             
             #iterating through rows. and saving every char in current row to a list
-            for row_index, row in enumerate(game_board):
-                    current_row.append(row[column])
-
-
-            #checking if win condition is met
-            if "XXX" == "".join(current_row):
-                  print("You won! (row win)")
-                  current_row.clear()
-                  return True
-            else:
-                current_row.clear()
-             
+            for row in game_board:
+                if row[0] == symbol and row[1] == symbol and row[2] == symbol:
+                    return True
 
 main()
