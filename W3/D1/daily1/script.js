eaa1989d29a3b81da5bd85bf7710d8180400178d
Arefaris <arefaris6969@gmail.com>
2025-05-25 15:25:50 +0300

    daily1

--- W3/D1/daily1/script.js
+++ W3/D1/daily1/script.js
@@ -1,16 +1,13 @@
 let sentence = "The movie is not that bad, I like it"
-
+//let sentence = "This movie is not so bad !"
+//let sentence = "This dinner is bad !"
 let wordNot = sentence.indexOf("not")
 let wordBad = sentence.lastIndexOf("bad")
 let new_sentence;
 
-
-
-if (wordNot < wordBad && wordNot == -1 && wordBad == -1){
-    new_sentence = sentence.slice(0, wordNot) + " " + "good" + sentence.slice(wordBad + 3)
+if (wordNot < wordBad && wordNot != -1 && wordBad != -1){
+    new_sentence = sentence.slice(0, wordNot) + "good" + sentence.slice(wordBad + 3)
     console.log(new_sentence)
-
-    
 } else {
     console.log(sentence);
 }
