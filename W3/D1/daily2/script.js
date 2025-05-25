7eb6fc47683a3075f0f1b9ff5fd800c74b202453
Arefaris <arefaris6969@gmail.com>
2025-05-25 15:55:12 +0300

    daily2

---
+++ W3/D1/daily2/script.js
@@ -0,0 +1,15 @@
+star = "*"
+ for (i=0; i < 6; i++){
+     console.log(star)
+     star = star + "*"
+ }
+
+for (i=0; i < 6; i++){
+    console.log(star)
+    for (i=0; i < 5; i++){
+        star = star + "*"
+        console.log(star)
+    }
+        
+    
+}
