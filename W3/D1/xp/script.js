7a579012f3e0e4033015938a0506221a3f5b1612
Arefaris <111435464+Arefaris@users.noreply.github.com>
2025-05-27 15:08:12 +0300

    fix

--- W3/D1/xp/script.js
+++ W3/D1/xp/script.js
@@ -1,125 +1,92 @@
-const people = ["Greg", "Mary", "Devon", "James"];
+//part 1
+let people = ["Greg", "Mary", "Devon", "James"];
 
-// part 1
-// 1
-greg = people.shift()
-console.log(greg);
-console.log(people);
+people.shift();
+people[people.indexOf("James")] = "Jason";
+people.push("Yourname");
 
-// 2
-people[2] = "Jason"
-console.log(people);
+console.log(people.indexOf("Mary"));
 
-// 3
+let peopleCopy = people.slice(1, 3);
 
-people.push("Vladislav")
-console.log(people);
+console.log(people.indexOf("Foo"));
 
-// 4
-console.log(people.indexOf("Mary"))
+let last = people[people.length - 1];
+console.log(last);
 
-// 5
-
-let new_people = people.slice(1,3)
-console.log(new_people)
-
-// 6
-console.log(people.indexOf("Foo")) // it returns - 1 because the value is not in the array
-
-// 7
-let last = people[people.length - 1]
-console.log(last)
-
-// part 2
-for (let i=0; i < people.length; i++){
-    //1
-    console.log(people[i])
-
-    //2
-    if (people[i] === "Devon"){
-        break
-    }
+//part 2
+for (let person of people) {
+  console.log(person);
 }
 
-// Ex 2
-
-const colors = ["black", "yellow", "red", "blue", "green"]
-const suffixes = ["st", "nd", "rd", "th", "th"]
-for (i=0; i < colors.length; i++){
-
-    console.log(`My #${i+1}${suffixes[i]} choise is ${colors[i]}`)
+for (let person of people) {
+  console.log(person);
+  if (person === "Devon") break;
 }
 
-// Ex 3
-// let user_input = prompt("Number?")
+//ex2
+const colors = ["blue", "red", "green", "black", "white"];
+const suffixes = ["st", "nd", "rd", "th", "th"];
 
-// while (user_input < 10){
-//     user_input = prompt("Number?")
-// }
+for (let i = 0; i < colors.length; i++) {
+  console.log(`My ${i + 1}${suffixes[i]} choice is ${colors[i]}`);
+}
 
-// ex 4
+//ex3 
+let num;
+do {
+  num = prompt("Enter a number");
+} while (Number(num) < 10);
+
+//ex4
 
 const building = {
-    numberOfFloors: 4,
-    numberOfAptByFloor: {
-        firstFloor: 3,
-        secondFloor: 4,
-        thirdFloor: 9,
-        fourthFloor: 2,
-    },
-    nameOfTenants: ["Sarah", "Dan", "David"],
-    numberOfRoomsAndRent:  {
-        sarah: [3, 990],
-        dan:  [4, 1000],
-        david: [1, 500],
-    },
+  numberOfFloors: 4,
+  numberOfAptByFloor: {
+    firstFloor: 3,
+    secondFloor: 4,
+    thirdFloor: 9,
+    fourthFloor: 2,
+  },
+  nameOfTenants: ["Sarah", "Dan", "David"],
+  numberOfRoomsAndRent: {
+    sarah: [3, 990],
+    dan: [4, 1000],
+    david: [1, 500],
+  },
+};
+
+console.log(building.numberOfFloors);
+console.log(building.numberOfAptByFloor.firstFloor + building.numberOfAptByFloor.thirdFloor);
+console.log(building.nameOfTenants[1], building.numberOfRoomsAndRent.dan[0]);
+
+let total = building.numberOfRoomsAndRent.sarah[1] + building.numberOfRoomsAndRent.david[1];
+if (total > building.numberOfRoomsAndRent.dan[1]) {
+  building.numberOfRoomsAndRent.dan[1] = 1200;
 }
 
-console.log(building.numberOfFloors)
-console.log(building.numberOfAptByFloor.firstFloor)
-console.log(building.numberOfAptByFloor.secondFloor)
-console.log(building.nameOfTenants[1], building.numberOfRoomsAndRent.dan[0] )
+//ex5
+const family = { dad: "John", mom: "Jane", son: "Mike" };
 
-let sara_rent = building.numberOfRoomsAndRent.sarah[1];
-let david_rent = building.numberOfRoomsAndRent.david[1];
-let dan_rent = building.numberOfRoomsAndRent.dan[1];
-
-if (sara_rent + david_rent > dan_rent){
-    building.numberOfRoomsAndRent.dan[1] += 200
+for (let key in family) {
+  console.log(key);
 }
 
-console.log(building.numberOfRoomsAndRent.dan[1])
-
-// ex 5
-let family = {
-    dad: "bob",
-    mom: "patrisha",
-    grandad: "michael"
+for (let key in family) {
+  console.log(family[key]);
 }
 
-for (const member in family){
-    console.log(member)
-    console.log(family[member])
+//ex6
+const details = { my: 'name', is: 'Rudolf', the: 'reindeer' };
+let sentence = "";
+
+for (let key in details) {
+  sentence += key + " " + details[key] + " ";
 }
+console.log(sentence.trim());
 
 
-// ex 6
-const details = {
-  my: 'name',
-  is: 'Rudolf',
-  the: 'reindeer'
-}
-
-for (const detail in details){
-    console.log(`${detail} ${details[detail]}`)
-}
-
-// ex 7
+//ex7
 const names = ["Jack", "Philip", "Sarah", "Amanda", "Bernard", "Kyle"];
-names.sort()
-
-let secret_society_name = ""
-for (const name of names){
-    secret_society_name += name[0]
-}
-console.log(secret_society_name)
+const secretName = names.map(name => name[0]).sort().join('');
+console.log(secretName); // "ABJKPS"
