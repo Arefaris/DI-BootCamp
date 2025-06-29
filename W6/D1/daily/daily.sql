5ba6a6f59d425030fb775fd13a534bb7a9f50c0f
Arefaris <111435464+Arefaris@users.noreply.github.com>
2025-06-29 13:28:59 +0300

    daily

---
+++ W6/D1/daily/daily.sql
@@ -0,0 +1,10 @@
+-- SELECT COUNT(*) FROM actors
+
+-- SELECT * from actors
+
+-- INSERT INTO actors (first_name, last_name, birthdate, number_oscars)
+-- VALUES ('Arnold',  '1999-07-21', 0);
+
+-- SELECT * from actors
+
+-- it will be an error since our colums cant be null
